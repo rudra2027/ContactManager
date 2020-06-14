01ea1ae1e77801f4e1cd5e0b4ad2e63ee7ddda26
rudra2027 <adarsh.ag15@gmail.com>
2020-06-14 10:28:47 +0530

    Updates

---
+++ precache-manifest.65ce22c2f3e8e3415844d7b89477a312.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2798fd968b1b2b55cd16e2de24048f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fce06d6568ed79f8a40d",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "44d028ab75040076d8bc",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fce06d6568ed79f8a40d",
+    "url": "/contactmanager/static/js/2.53fc696e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.53fc696e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d028ab75040076d8bc",
+    "url": "/contactmanager/static/js/main.d578ea68.chunk.js"
+  },
+  {
+    "revision": "31aa565394677d9739a4",
+    "url": "/contactmanager/static/js/runtime-main.ab1f59d6.js"
+  }
+]);
